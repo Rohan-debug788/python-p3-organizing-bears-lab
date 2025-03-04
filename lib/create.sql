8a3aacdf65c26e203d15c4d9ea053133eece12a5
Rohan Kibet <degaullerohan@gmail.com>
2025-03-04 18:43:29 +0300

    add sql

---
+++ lib/create.sql
@@ -0,0 +1,9 @@
+CREATE TABLE bears (
+  id INTERGER,
+  name TEXT,
+  age INTERGER,
+  sex TEXT,
+  color TEXT,
+  temperament TEXT,
+  alive BOOLEAN
+);
---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears ( 1, name, age, sex, color, temperament, alive) VALUES ('Mr.Chocolate', 6, 'M', 'Brown', 'light', yes);
+INSERT INTO bears ( 2, name, age, sex, color, temperament, alive) VALUES ('Rowdy', 6, 'M', 'Black', 'high', yes);
+INSERT INTO bears ( 3, name, age, sex, color, temperament, alive) VALUES ('Tabitha', 6, 'M', 'Brown', 'medium', yes);
+INSERT INTO bears ( 4, name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 6, 'M', 'Grey', 'high', yes);
+INSERT INTO bears ( 5, name, age, sex, color, temperament, alive) VALUES ('Melissa', 6, 'M', 'White', 'high', no);
+INSERT INTO bears ( 6, name, age, sex, color, temperament, alive) VALUES ('Grinch', 6, 'M', 'White', 'medium', yes);
+INSERT INTO bears ( 7, name, age, sex, color, temperament, alive) VALUES ('Wendy', 6, 'M', 'Black', 'low', no);
+INSERT INTO bears ( 8, name, age, sex, color, temperament, alive) VALUES (NULL, 6, 'M', 'Brown', 'high', yes);
